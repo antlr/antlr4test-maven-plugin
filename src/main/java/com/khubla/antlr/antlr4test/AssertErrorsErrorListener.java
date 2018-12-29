922cae4926ee55845a6845be0f9867d5f01a32a5
Mario-S <spindizzy@outlook.com>
2018-12-29 22:52:39 +0100

    fix when expected input contains line break

--- src/main/java/com/khubla/antlr/antlr4test/AssertErrorsErrorListener.java
+++ src/main/java/com/khubla/antlr/antlr4test/AssertErrorsErrorListener.java
@@ -19,31 +19,20 @@ public class AssertErrorsErrorListener extends BaseErrorListener {
    protected static final String LITERAL_BACKSLASH_R = "\\\\r";
    protected static final String LITERAL_BACKSLASH_N_PLACEHOLDER = "literal-backslash-n";
    protected static final String LITERAL_BACKSLASH_N = "\\\\n";
-   protected List<String> errorMessages = new ArrayList<String>();
+   protected List<String> errorMessages = new ArrayList<>();
 
    public void assertErrors(File errorMessagesFile, String encoding) throws AssertErrorsException {
       if (!errorMessages.isEmpty()) {
          List<String> expectedErrorMessages = null;
+         String errorMessageFileName = errorMessagesFile.getName();
          try {
             expectedErrorMessages = FileUtil.getNonEmptyLines(errorMessagesFile, encoding);
          } catch (final FileNotFoundException ex) {
-            throw new AssertErrorsException(String.format("found %d errors, but missing file %s", errorMessages.size(), errorMessagesFile.getName()), ex);
+            throw new AssertErrorsException(String.format("found %d errors, but missing file %s", errorMessages.size(), errorMessageFileName), ex);
          } catch (final IOException ex) {
-            throw new AssertErrorsException(String.format("found %d errors, unable to read file %s", errorMessages.size(), errorMessagesFile.getName()), ex);
+            throw new AssertErrorsException(String.format("found %d errors, unable to read file %s", errorMessages.size(), errorMessageFileName), ex);
          }
-         expectedErrorMessages = replacePlaceholders(expectedErrorMessages);
-         final Object[] expectedErrors = expectedErrorMessages.toArray();
-         final Object[] errors = errorMessages.toArray();
-         if (expectedErrors.length != errors.length) {
-            throw new AssertErrorsException(String.format("%s : expected %d errors, but was %d errors", errorMessagesFile.getName(), expectedErrors.length, errors.length));
-         }
-         for (int i = 0; i < expectedErrors.length; i = i + 1) {
-            final Object expectedError = expectedErrors[i];
-            final Object error = errors[i];
-            if (!expectedError.equals(error)) {
-               throw new AssertErrorsException(String.format("%s : expected (%s), but was (%s)", errorMessagesFile.getName(), expectedError, error));
-            }
-         }
+         asserts(expectedErrorMessages, errorMessageFileName);
       } else {
          if (errorMessagesFile.exists()) {
             throw new AssertErrorsException(String.format("no errors found, but errors file exists %s", errorMessagesFile.getAbsolutePath()));
@@ -51,8 +40,25 @@ public class AssertErrorsErrorListener extends BaseErrorListener {
       }
    }
 
+    void asserts(List<String> expectedErrorMessages, String errorMessageFileName) throws AssertErrorsException {
+        List<String> expectedErrors = replacePlaceholders(expectedErrorMessages);
+        List<String> actualErrors = replacePlaceholders(errorMessages);
+        int expectedSize = expectedErrors.size();
+        int actualSize = actualErrors.size();
+        if (expectedSize != actualSize) {
+            throw new AssertErrorsException(String.format("%s : expected %d errors, but was %d errors", errorMessageFileName, expectedSize, actualSize));
+        }
+        for (int i = 0; i < expectedSize; i++) {
+            final String expectedError = expectedErrors.get(i);
+            final String error = actualErrors.get(i);
+            if (!expectedError.equals(error)) {
+                throw new AssertErrorsException(String.format("%s : expected (%s), but was (%s)", errorMessageFileName, expectedError, error));
+            }
+        }
+    }
+
    protected List<String> replacePlaceholders(List<String> stringList) {
-      final List<String> replacedStringList = new ArrayList<String>();
+      final List<String> replacedStringList = new ArrayList<>();
       for (final String vString : stringList) {
          replacedStringList.add(replacePlaceholders(vString));
       }
---
+++ src/test/java/com/khubla/antlr/antlr4test/AssertErrorsErrorListenerTest.java
@@ -0,0 +1,54 @@
+/*
+ * 
+ */
+package com.khubla.antlr.antlr4test;
+
+import java.io.File;
+import java.util.Collections;
+import java.util.List;
+import org.antlr.v4.runtime.RecognitionException;
+import org.antlr.v4.runtime.Recognizer;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author mario.schroeder
+ */
+public class AssertErrorsErrorListenerTest {
+    
+    private AssertErrorsErrorListener classUnderTest;
+    
+    @Before
+    public void setUp() {
+        classUnderTest = new AssertErrorsErrorListener();
+    }
+
+    /**
+     * Test of replacePlaceholders method, of class AssertErrorsErrorListener.
+     */
+    @Test
+    public void testReplacePlaceholders_Unchanged() {
+        String line = "mismatched input '\\n' expecting 'foo'";
+        List<String> lines = Collections.singletonList(line);
+        classUnderTest.replacePlaceholders(lines);
+        assertEquals(lines.get(0), line);
+    }
+    
+    /**
+     * Test of replacePlaceholders method, of class AssertErrorsErrorListener.
+     */
+    @Test
+    public void testAssertErrors_Matching() throws AssertErrorsException {
+        String line = "mismatched input '\\n' expecting 'foo'";
+        classUnderTest.syntaxError(null, null, 1, 7, line, null);
+        
+        String expected = "line 1:7 "+line;
+        List<String> lines = Collections.singletonList(expected);
+        classUnderTest.asserts(lines, getClass().getSimpleName());
+        assertEquals(lines.get(0), expected);
+    }
+    
+}
